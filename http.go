4be8c23753f668c5f162f86d2bc706be19f8454a
au742739 <au742739@uni.au.dk>
2026-09-03 11:13:45 +0200

    ok

---
+++ http.go
@@ -0,0 +1,16 @@
+package raft
+
+import "net/http"
+
+func (n *Node) Get(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+func (n *Node) Create(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+func (n *Node) Update(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+func (n *Node) Delete(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
--- start.sh
+++ start.sh
@@ -6,8 +6,8 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-go run "$DIR/cmd/raft-node/main.go" --id 1 --port 8081 --peers=localhost:8082,localhost:8083 "$@" &
-go run "$DIR/cmd/raft-node/main.go" --id 2 --port 8082 --peers=localhost:8081,localhost:8083 "$@" &
-go run "$DIR/cmd/raft-node/main.go" --id 3 --port 8083 --peers=localhost:8081,localhost:8082 "$@" &
+go run "$DIR/cmd/raft-node/main.go" --id 1 --grpc-port 8081 --http-port 8091 --peers=localhost:8082,localhost:8083 "$@" &
+go run "$DIR/cmd/raft-node/main.go" --id 2 --grpc-port 8082 --http-port 8092 --peers=localhost:8081,localhost:8083 "$@" &
+go run "$DIR/cmd/raft-node/main.go" --id 3 --grpc-port 8083 --http-port 8093 --peers=localhost:8081,localhost:8082 "$@" &
 
 wait
